3b050844c2135da6d70ebc0d1f9e46e97ba1c9b5
vic-jun <vjunco304@alumno.unlam.edu.ar>
2025-06-09 21:07:10 -0300

    Se soluciona muestra de errores en vistas inicio y registro.

--- src/app/registro/registro.ts
+++ src/app/registro/registro.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css'
 })
@@ -19,6 +20,11 @@ export class RegistroComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
   onRegister() {
+    this.error = '';
+    if (!this.email || !this.password) {
+    this.error = 'Por favor, completa todos los campos.';
+    return;
+    }
     if (this.password !== this.confirmPassword) {
       this.error = 'Las contraseñas no coinciden';
       return;
